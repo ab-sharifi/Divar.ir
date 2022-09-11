5d4daf148da716ea2dd46730931b8cd4081e72d0
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-09-11 13:46:12 +0430

    added header moda

--- Python/app.py
+++ Python/app.py
@@ -1,1 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def inedx():
+    return render_template('divar-index-home/index.html')
