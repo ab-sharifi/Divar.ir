8fba0ae6098871d8ff8e55770179ea1294fb19a6
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-09-12 18:36:47 +0430

    session city add

--- Python/divar/config.py
+++ Python/divar/config.py
@@ -1,4 +1,5 @@
 class config:
+    # created by secrets lib in python for test
     SECRET_KEY = 'a15a278b4567a92d8e7ae65c693e7ab7dba8fc01706d68d04d6a14dc9f9c3666'
     TEMPLATES_AUTO_RELOAD = True
     SESSION_PERMANENT = False
