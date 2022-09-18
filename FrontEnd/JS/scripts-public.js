41387d71178acc0f8f74773753e33e1ebdf5e17a
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2022-09-18 11:31:14 +0430

    added public js

--- FrontEnd/JS/scripts-public.js
+++ FrontEnd/JS/scripts-public.js
@@ -1,3 +1,4 @@
+var city_tag_names = document.querySelector("#city-tag-names");
 
 function selectCityfromArea(city_id, checkedState) {
     if (city_tag_names.innerHTML.toString().includes("حداقل یک شهر را انتخاب کنید."))
@@ -5,14 +6,13 @@ function selectCityfromArea(city_id, checkedState) {
 
     if (checkedState) {
         city_tag_names.innerHTML += `
-    
+
         <div class="d-flex align-items-center bg-transparent text-muted border-color-brand border-radius-30 px-2 pt-2 me-2" id="city-tag-` + city_id + `">
         <h6 class="text-danger fs-8">` + city_id + `</h6>
         <button
           class="bg-transparent text-danger border-0 border-radius-circle bg-danger-hover fs-10 fw-bold px-2 py-1 ms-1"
-          onclick="removeElement('`+ 'city-tag-' + city_id + `'); check_cityTagNames(); document.querySelector('` + '#checkbox-tag-' + city_id + `').checked = false;">X</button>
+          onclick="removeElement('`+ 'city-tag-' + city_id + `'); document.querySelector('` + '#checkbox-tag-' + city_id + `').checked = false;">X</button>
       </div>
-        
         `;
 
         document.querySelector("#btnSelectCities").disabled = false;
@@ -28,6 +28,7 @@ function removeElement(elemnt_id) {
 }
 
 function check_cityTagNames() {
+
     if (!city_tag_names.innerHTML.includes("div"))
     {
         city_tag_names.innerHTML = "<h6 class='text-muted fs-8 pt-2'>حداقل یک شهر را انتخاب کنید.</h6>";
